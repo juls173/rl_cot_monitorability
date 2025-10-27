5f6503c2656d051d223916fbd309136be111386e
Julian Huang <ilikevegetables12@gmail.com>
2025-10-27 00:39:41 -0500

    Increase micro batch sizes

--- scripts/run_grpo_LoRA.sh
+++ scripts/run_grpo_LoRA.sh
@@ -10,14 +10,14 @@ REWARD_FN_PATH=/workspace/rl_cot_monitorability/scripts/gsm8k_reward.py
 REWARD_FN_NAME=compute_score
 ACTOR=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
 PROJECT=verl_gsm8k_v2
-EXP=r1qwen15b_grpo_temp11_topp95_entropy001_kl0001
+EXP=r1qwen15b_grpo_temp11_topp95_entropy0005_kl0001
 
 python3 -m verl.trainer.main_ppo \
   algorithm.adv_estimator=grpo \
   algorithm.use_kl_in_reward=False \
   data.train_files=${DATA_DIR}/train.parquet \
   data.val_files=${DATA_DIR}/test.parquet \
-  data.train_batch_size=128 \
+  data.train_batch_size=256 \
   data.max_prompt_length=180 \
   data.max_response_length=1024 \
   data.filter_overlong_prompts=True \
@@ -30,9 +30,9 @@ python3 -m verl.trainer.main_ppo \
   actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=10 \
   actor_rollout_ref.actor.ppo_mini_batch_size=64 \
   actor_rollout_ref.actor.use_kl_loss=True \
-  actor_rollout_ref.actor.kl_loss_coef=0.001 \
+  actor_rollout_ref.actor.kl_loss_coef=0.005 \
   actor_rollout_ref.actor.kl_loss_type=low_var_kl \
-  actor_rollout_ref.actor.entropy_coeff=0.01 \
+  actor_rollout_ref.actor.entropy_coeff=0.005 \
   actor_rollout_ref.actor.strategy=fsdp2 \
   actor_rollout_ref.model.enable_gradient_checkpointing=True \
   actor_rollout_ref.actor.fsdp_config.param_offload=False \
@@ -42,7 +42,7 @@ python3 -m verl.trainer.main_ppo \
   actor_rollout_ref.rollout.temperature=1.1 \
   actor_rollout_ref.rollout.top_p=0.95 \
   actor_rollout_ref.rollout.n=5 \
-  actor_rollout_ref.rollout.gpu_memory_utilization=0.91 \
+  actor_rollout_ref.rollout.gpu_memory_utilization=0.87 \
   actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=20 \
   actor_rollout_ref.rollout.load_format="safetensors" \
   actor_rollout_ref.rollout.layered_summon=True \
@@ -58,6 +58,6 @@ python3 -m verl.trainer.main_ppo \
   trainer.experiment_name=${EXP} \
   trainer.n_gpus_per_node=2 \
   trainer.nnodes=1 \
-  trainer.save_freq=39 \
-  trainer.test_freq=13 \
+  trainer.save_freq=10 \
+  trainer.test_freq=3 \
   trainer.total_epochs=1 \
